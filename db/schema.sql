6d1178f0ddf477301852e5af8b79d9ab10e77bf6
Eric Choi <39499807+ericjchoi@users.noreply.github.com>
2020-03-10 18:34:38 -0700

    code cleaned /db/schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,16 +8,3 @@ burger_name VARCHAR (100) NOT NULL,
 devoured BOOLEAN DEFAULT FALSE,
 PRIMARY KEY (id)
 );
-
--- for JAWSDB in heroku
--- DROP DATABASE IF EXISTS bkofhurmyio3tyjm;
--- CREATE DATABASE bkofhurmyio3tyjm;
--- USE bkofhurmyio3tyjm;
-
--- CREATE TABLE burgers (
--- id INT AUTO_INCREMENT NOT NULL,
--- burger_name VARCHAR (100) NOT NULL,
--- devoured BOOLEAN DEFAULT FALSE,
--- createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
--- PRIMARY KEY (id)
--- );
